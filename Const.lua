ffb5241b9c59d5fde2cc2dcae0950f7b16acdb37
VCell@win <roguevcell@gmail.com>
2026-03-09 20:45:07 +0800

    new file:   Const.lua 	modified:   LogFrame.lua 	modified:   OptionPanel.lua 	modified:   PPBest.lua 	modified:   PPBest.toc

---
+++ Const.lua
@@ -0,0 +1,17 @@
+local _, PPBest = ...
+
+PPBestConfig = PPBestConfig or {
+    hotkey = "F8",
+    mode = "default", --default,ai
+    assist_target = "",
+}
+
+PPBest.Const = {
+    MODE_DEFAULT = "default",
+    MODE_AI = "AI",
+    MODE_ASSIST = "assist",
+    MODE_WANT_EXP = "want_exp",
+    MODE_WANT_WIN = "want_win",
+    MODE_WANT_PET_LEVEL = "want_pet_level"
+}
+
